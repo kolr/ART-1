5c4a522603ae4735cd1b6e70143ed2e99fcaa194
kolr <kol-radik@yandex.ru>
2016-06-04 16:22:21 +0300

    Initialized constants and created Comparative Layer

---
+++ src/main/java/org/Constants.java
@@ -0,0 +1,36 @@
+package org;
+
+/**
+ * 04.06.2016
+ * Created by Rodion.
+ */
+public class Constants {
+    /**
+     * Constant for initializing of the bottom-top weights.
+     */
+    private static final int L = 2;
+
+    /**
+     * Max. number of classes.
+     */
+    private static final int M = 4;
+
+    /**
+     * Size of  input vectors.
+     */
+    private static final int N = 5;
+
+    /**
+     * Number of input objects.
+     */
+    private static final int Q = 4;
+
+    /**
+     * Similarity parameter.
+     */
+    private static final double P = 0.45;
+
+    private Constants() {
+        super();
+    }
+}
---
+++ src/main/java/org/components/ComparativeLayer.java
@@ -0,0 +1,9 @@
+package org.components;
+
+/**
+ * 04.06.2016
+ * Created by Rodion.
+ */
+public class ComparativeLayer {
+
+}
