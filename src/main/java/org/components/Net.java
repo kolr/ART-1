887853f0afde6cbf1723397d5d3d61c986b35aff
kolr <kol-radik@yandex.ru>
2016-06-04 18:19:29 +0300

    GOne implemented

--- src/main/java/org/components/Net.java
+++ src/main/java/org/components/Net.java
@@ -14,7 +14,7 @@ public class Net {
     }
 
     private void sendObjectTo() {
-        
+
     }
 
 
--- src/main/java/org/components/neurons/managing/GOne.java
+++ src/main/java/org/components/neurons/managing/GOne.java
@@ -9,5 +9,25 @@ package org.components.neurons.managing;
  * Neuron-receiver to activate comparative layer if object not empty.
  */
 public class GOne {
-//    public //
+    private boolean inhibited;
+
+    public boolean isInhibited() {
+        return inhibited;
+    }
+
+    public void setInhibited(boolean inhibited) {
+        this.inhibited = inhibited;
+    }
+
+    public int activate(int[] object) {
+        if (!inhibited) {
+            for (int i = 0; i < object.length; i++) {
+                if (object[i] == 1) {
+                    return 1;
+                }
+            }
+            return 0;
+        }
+        return 0;
+    }
 }
