cabce3b4e30fc5b88e4bedc374442e8a4cbfb134
kolr <kol-radik@yandex.ru>
2016-06-04 16:46:25 +0300

    Created recognition Layer

---
+++ src/main/java/org/components/RecognitionLayer.java
@@ -0,0 +1,29 @@
+package org.components;
+
+import org.Constants;
+
+/**
+ * 04.06.2016
+ * Created by Rodion.
+ */
+public class RecognitionLayer {
+    private int[] recognitionLayerOutput;
+    private double[][] topBottomWeights;
+
+    public RecognitionLayer() {
+        recognitionLayerOutput = new int[Constants.M];
+        topBottomWeights = new double[Constants.M][Constants.N];
+    }
+
+    public void initializeTopBottomWeights() {
+        for (int i = 0; i < Constants.M; i++) {
+            for (int j = 0; j < Constants.N; j++) {
+                this.topBottomWeights[i][j] = (double) 1;
+            }
+        }
+    }
+
+    public double[][] getTopBottomWeights() {
+        return this.topBottomWeights;
+    }
+}
